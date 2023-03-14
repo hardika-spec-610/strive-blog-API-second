25394c9aa136faf52ff7f77f93fbe3dc76d5b8fa
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-03-14 15:47:07 +0100

    update and delete blog post

--- src/api/blogs/blogIndex.js
+++ src/api/blogs/blogIndex.js
@@ -38,5 +38,51 @@ blogsRouter.get("/:blogId", async (req, res, next) => {
     next(error);
   }
 });
+blogsRouter.put("/:blogId", async (req, res, next) => {
+  try {
+    const updatedBlog = await blogPostModel.findByIdAndUpdate(
+      req.params.blogId, // WHO
+      req.body, // HOW
+      { new: true, runValidators: true } // OPTIONS. By default findByIdAndUpdate returns the record pre-modification. If you want to get the newly updated one you shall use new: true
+      // By default validation is off in the findByIdAndUpdate --> runValidators: true
+    );
+
+    // ********************************************** ALTERNATIVE METHOD **********************************
+    // const blog = await blogPostModel.findById(req.params.blogId);
+    // // When you do a findById, findOne,... you get back a MONGOOSE DOCUMENT which is NOT a normal JS Object!
+    // // It is an object with superpowers, for instance it has .save() method that could be very useful in some cases
+    // blog.category = "any";
+
+    // await blog.save();
+
+    // ****************************************************************************************************
+    if (updatedBlog) {
+      res.send(updatedBlog);
+    } else {
+      next(
+        createHttpError(404, `Blog with id ${req.params.blogId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogsRouter.delete("/:blogId", async (req, res, next) => {
+  try {
+    const deletedBlog = await blogPostModel.findByIdAndDelete(
+      req.params.blogId
+    );
+    if (deletedBlog) {
+      res.status(204).send();
+    } else {
+      next(
+        createHttpError(404, `Blog with id ${req.params.blogId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default blogsRouter;
