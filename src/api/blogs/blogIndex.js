e0fabae49a827a7ade73d31cff015a34659db74e
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-03-15 16:26:21 +0100

    pagination

--- src/api/blogs/blogIndex.js
+++ src/api/blogs/blogIndex.js
@@ -19,8 +19,23 @@ blogsRouter.post("/", async (req, res, next) => {
 });
 blogsRouter.get("/", async (req, res, next) => {
   try {
-    const blogs = await blogPostModel.find();
-    res.send(blogs);
+    console.log("req.query", req.query);
+    console.log("q2m", q2m(req.query));
+    const mongoQuery = q2m(req.query);
+    //  price: '>10' should be converted somehow into price: {$gt: 10}
+    const blogs = await blogPostModel
+      .find(mongoQuery.criteria, mongoQuery.options.fields)
+      .limit(mongoQuery.options.limit)
+      .skip(mongoQuery.options.skip)
+      .sort(mongoQuery.options.sort);
+    const total = await blogPostModel.countDocuments(mongoQuery.criteria);
+    // no matter the order of usage of these methods, Mongo will ALWAYS apply SORT then SKIP then LIMIT
+    res.send({
+      links: mongoQuery.links(process.env.LOCAL_URL + "/blogPosts", total),
+      total,
+      numberOfPages: Math.ceil(total / mongoQuery.options.limit),
+      blogs,
+    });
   } catch (error) {
     next(error);
   }
