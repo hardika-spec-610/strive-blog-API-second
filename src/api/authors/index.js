39d4c31bfb16292fc06ed779793bbf267e51bc53
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-04-04 15:38:50 +0200

    create adminMiddleware

--- src/api/authors/index.js
+++ src/api/authors/index.js
@@ -7,6 +7,7 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 import { triggerBadRequest } from "../validation.js";
 import { basicAuthMiddleware } from "../../lib/auth/basic.js";
+import { adminOnlyMiddleware } from "../../lib/auth/admin.js";
 
 const authorsRouter = Express.Router();
 
@@ -29,31 +30,36 @@ authorsRouter.post("/", triggerBadRequest, async (req, res, next) => {
   }
 });
 
-authorsRouter.get("/", basicAuthMiddleware, async (req, res, next) => {
-  try {
-    console.log("req.query", req.query);
-    console.log("q2m", q2m(req.query));
-    const mongoQuery = q2m(req.query);
-    //  price: '>10' should be converted somehow into price: {$gt: 10}
-    const authors = await AuthorsModel.find(
-      mongoQuery.criteria,
-      mongoQuery.options.fields
-    )
-      .limit(mongoQuery.options.limit)
-      .skip(mongoQuery.options.skip)
-      .sort(mongoQuery.options.sort);
-    const total = await AuthorsModel.countDocuments(mongoQuery.criteria);
-    // no matter the order of usage of these methods, Mongo will ALWAYS apply SORT then SKIP then LIMIT
-    res.send({
-      links: mongoQuery.links(process.env.LOCAL_URL + "/authors", total),
-      total,
-      numberOfPages: Math.ceil(total / mongoQuery.options.limit),
-      authors,
-    });
-  } catch (error) {
-    next(error);
+authorsRouter.get(
+  "/",
+  basicAuthMiddleware,
+  adminOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+      console.log("req.query", req.query);
+      console.log("q2m", q2m(req.query));
+      const mongoQuery = q2m(req.query);
+      //  price: '>10' should be converted somehow into price: {$gt: 10}
+      const authors = await AuthorsModel.find(
+        mongoQuery.criteria,
+        mongoQuery.options.fields
+      )
+        .limit(mongoQuery.options.limit)
+        .skip(mongoQuery.options.skip)
+        .sort(mongoQuery.options.sort);
+      const total = await AuthorsModel.countDocuments(mongoQuery.criteria);
+      // no matter the order of usage of these methods, Mongo will ALWAYS apply SORT then SKIP then LIMIT
+      res.send({
+        links: mongoQuery.links(process.env.LOCAL_URL + "/authors", total),
+        total,
+        numberOfPages: Math.ceil(total / mongoQuery.options.limit),
+        authors,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 authorsRouter.get("/:authorId", basicAuthMiddleware, async (req, res, next) => {
   try {
